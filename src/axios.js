b45742767a317e52b7780055c8bd60038250d389
Appiah Collins <appiahc86@gmail.com>
2023-03-12 11:06:44 +0000

    updated

--- src/axios.js
+++ src/axios.js
@@ -3,9 +3,9 @@ import axios from "axios";
 import {useHomeStore} from "./store/home.js";
 
 
-axios.defaults.baseURL = "http://localhost:3000";
+// axios.defaults.baseURL = "http://localhost:3000";
 // axios.defaults.baseURL = "http://192.168.1.101:3000";
-// axios.defaults.baseURL = "https://test.christrevivalch.org";
+axios.defaults.baseURL = "https://test.christrevivalch.org";
 axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 axios.defaults.headers.patch['Content-Type'] = 'application/json; charset=utf-8';
 axios.defaults.headers.delete['Content-Type'] = 'application/json; charset=utf-8';
